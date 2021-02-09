884e4f3c35e47078784203f602bef81a4ae88422
rebeca-august <rebeca.cazacu@gmail.com>
2021-02-09 18:48:06 +0000

    finish reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -9,9 +9,53 @@ const reducer = (state, action) => {
     }
   }
   if (action.type === 'INCREASE') {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 }
+      }
+      return cartItem
+    })
+    return { ...state, cart: tempCart }
   }
-  if (action.type === 'INCREASE') {
+
+  if (action.type === 'DECREASE') {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 }
+        }
+        return cartItem
+      })
+      .filter((cartItem) => cartItem.amount !== 0)
+    return { ...state, cart: tempCart }
   }
+
+  if (action.type === 'GET_TOTAL') {
+    let { total, amount } = state.cart.reduce(
+      (cartTotal, cartItem) => {
+        const { price, amount } = cartItem
+        const itemTotal = price * amount
+
+        cartTotal.amount += amount
+        cartTotal.total += itemTotal
+
+        return cartTotal
+      },
+      { total: 0, amount: 0 },
+    )
+    total = parseFloat(total.toFixed(2))
+
+    return { ...state, total, amount }
+  }
+
+  if (action.type === 'LOADING') {
+    return { ...state, loading: true }
+  }
+
+  if (action.type === 'DISPLAY_ITEMS') {
+    return { ...state, cart: action.payload, loading: false }
+  }
+
   return state
 }
 
