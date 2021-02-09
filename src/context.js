2d24815ed83b8e6257e41ec115f7baf103043328
rebeca-august <rebeca.cazacu@gmail.com>
2021-02-09 19:21:49 +0000

    finish global context

--- src/context.js
+++ src/context.js
@@ -1,14 +1,13 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
 import reducer from './reducer'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
 const initialState = {
   loading: false,
-  cart: cartItems,
+  cart: [],
   total: 0,
   amount: 0,
 }
@@ -31,6 +30,21 @@ const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: 'DECREASE', payload: id })
   }
+
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' })
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  useEffect(() => {
+    dispatch({ type: 'GET_TOTAL' })
+  }, [state.cart])
 
   return (
     <AppContext.Provider
